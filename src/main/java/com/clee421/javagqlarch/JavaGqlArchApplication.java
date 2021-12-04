0280f689bb92b671553a85cc5fb7da30237e733f
Calvin Lee <clee421@gmail.com>
2021-12-04 18:30:59 -0500

    Initial Commit: code gen from start.spring.io

---
+++ src/main/java/com/clee421/javagqlarch/JavaGqlArchApplication.java
@@ -0,0 +1,13 @@
+package com.clee421.javagqlarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGqlArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGqlArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clee421/javagqlarch/JavaGqlArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clee421.javagqlarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGqlArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
