7ce6f3313b9290563991878564da6180a247dbe4
Calvin Lee <clee421@gmail.com>
2021-12-04 19:11:19 -0500

    Basic tutorial GQL setup

---
+++ src/main/java/com/clee421/javagqlarch/BookDetailsApplication.java
@@ -0,0 +1,11 @@
+package com.clee421.javagqlarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDetailsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookDetailsApplication.class, args);
+    }
+}
